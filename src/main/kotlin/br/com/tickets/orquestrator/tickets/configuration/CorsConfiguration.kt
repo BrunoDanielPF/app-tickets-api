832b63019d8ef869c955ff23121a2c8d9cad0e0f
bruno <frandalozzo@gmail.com>
2024-10-27 00:35:42 -0300

    configure cors configuration

---
+++ src/main/kotlin/br/com/tickets/orquestrator/tickets/configuration/CorsConfiguration.kt
@@ -0,0 +1,27 @@
+package br.com.tickets.orquestrator.tickets.configuration
+
+import org.hibernate.cfg.Environment
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CorsConfiguration: WebMvcConfigurer {
+
+    @Bean
+    fun webMvcConfigurer(environment: Environment) : WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "localhost"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
