aa71598868b9c73c47b320fdab863494323bc8c6
bruno <frandalozzo@gmail.com>
2024-10-27 00:46:55 -0300

    configure cors configuration

--- src/main/kotlin/br/com/tickets/orquestrator/tickets/configuration/CorsConfiguration.kt
+++ src/main/kotlin/br/com/tickets/orquestrator/tickets/configuration/CorsConfiguration.kt
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
-@Configuration
+//@Configuration
 class CorsConfiguration: WebMvcConfigurer {
 
     @Bean
--- src/main/kotlin/br/com/tickets/orquestrator/tickets/controller/QrCodeController.kt
+++ src/main/kotlin/br/com/tickets/orquestrator/tickets/controller/QrCodeController.kt
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -17,6 +18,7 @@ import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 @RestController
+@CrossOrigin(origins = ["*"])
 class QrCodeController(
     val qrCodeComponent: DefaultQrCodeComponent
 ) {
