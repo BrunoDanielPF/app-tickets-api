a2fd7dd00a14836d225b1d935a3f953481afeefb
bruno <frandalozzo@gmail.com>
2024-11-21 00:13:21 -0300

    removendo exception handler

--- src/main/kotlin/br/com/tickets/orquestrator/tickets/configuration/exception/CustomExceptionHandlerControllerAdvice.kt
+++
@@ -1,8 +0,0 @@
-package br.com.tickets.orquestrator.tickets.configuration.exception
-
-import org.springframework.web.bind.annotation.ControllerAdvice
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-
-@ControllerAdvice
-class CustomExceptionHandlerControllerAdvice : ResponseEntityExceptionHandler() {
-}
