860f2014a7205e68020647daa7f0cfe7b127dc15
BrunoDanielPF <frandalozzo@gmail.com>
2024-12-11 09:07:18 -0300

    teste

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,9 @@ java {
 
 repositories {
 	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { 
+		url = uri("https://repo.spring.io/milestone")
+	 }
 }
 
 dependencies {
